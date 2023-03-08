80df90b86b712a918382b9e7617a9fac7c962930
Jennifer Pajalla Bravo <jhenchem0210@gmail.com>
2023-03-09 04:09:34 +0800

    modified:   app/Http/Controllers/ConversationController.php 	deleted:    resources/views/about.blade.php 	modified:   resources/views/components/welcome.blade.php 	deleted:    resources/views/contact.blade.php 	modified:   resources/views/conversations/index.blade.php 	modified:   resources/views/dashboard.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/navigation-menu.blade.php 	deleted:    resources/views/policy.blade.php 	deleted:    resources/views/terms.blade.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,12 @@
+<x-guest-layout>
+
+    <div class="container">
+        <div class="card align-items-center">
+            <div class="row">
+                <div class="col-md-3 p-5"><img src="images/logo1.png" width=380 height=300 class="animate-bounce"/></div>
+            <div class="col-md-4 offset-md-4 ">
+                <h1 class="text-2xl">Contact Us</h1>
+                <p>Lorem ipsum,
+                
+            </div>
+</x-guest-layout>
